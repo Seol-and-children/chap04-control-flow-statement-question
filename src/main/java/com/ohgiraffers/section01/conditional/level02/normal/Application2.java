6c944db5c7e64952195cb46267557b94ba101528
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2023-05-06 00:34:10 +0900

    section01_level02_02 if문을 활용하여 해결. double을 활용하려다 실패했다. 추후 다시 생각해보자.

--- src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/section01/conditional/level02/normal/Application2.java
@@ -1,10 +1,12 @@
 package com.ohgiraffers.section01.conditional.level02.normal;
 
+import java.util.Scanner;
+
 public class Application2 {
-	
+
 	public static void main(String[] args) {
-		
-		/* BMI(신체질량지수)를 계산하고, 계산된 값에 따라 
+
+		/* BMI(신체질량지수)를 계산하고, 계산된 값에 따라
 		 * 저체중(20 미만)인 경우 "당신은 저체중 입니다.", 
 		 * 정상체중(20이상 25미만)인 경우 "당신은 정상체중 입니다.", 
 		 * 과제충(25이상 30미만)인 경우 "당신은 과체중 입니다.", 
@@ -14,7 +16,19 @@ public class Application2 {
 		 * 
 		 * 계산 예시) BMI = 67 / (1.7 * 1.7)
 		 * */
-		
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("체중을 입력하세요 : ");
+		int weight = sc.nextInt();
+		System.out.print("신장을 입력하세요(cm) : ");
+		int high = sc.nextInt();
+
+		int BMI = (weight / ((high * high)/10000));
+		System.out.println(BMI + "키와무게" + high);
+		if (BMI < 20) System.out.println("당신은 저체중 입니다.");
+		else if (BMI < 25 && BMI >= 20) System.out.println("당신은 정상체중 입니다.");
+		else if (BMI < 30 && BMI >= 25) System.out.println("당신은 과체중 입니다.");
+		else System.out.println("당신은 비만 입니다.");
 	}
-	
+
 }
