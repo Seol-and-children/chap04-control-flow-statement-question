d4dd1a79107bb50fd374ea0f17237b8d6fd74229
DESKTOP-OJVF8KK\seol <tjfrhkdwls12@naver.com>
2023-05-06 00:37:25 +0900

    section02_level02_01 for문을 활용해 해결

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application1.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level02/normal/Application1.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.section02.looping_and_branching.level02.normal;
 
+import java.util.Scanner;
+
 public class Application1 {
 
 	public static void main(String[] args) {
@@ -18,7 +20,14 @@ public class Application1 {
 		 * 3 : l
 		 * 4 : e
 		 * */
-		
+		int i = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("문자열를 입력하세요 : ");
+		String main = sc.next();
+		for(char x : main.toCharArray()){
+			System.out.println(i + " : " + x);
+			i++;
+		}
 	}
 
 }
