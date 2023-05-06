6dd327691dc0cc22424f1b7b9b2531bca53cd8c6
busker362 <68013292+busker362@users.noreply.github.com>
2023-05-06 20:41:49 +0900

    2023-05-04 수업 내용 문제 중 코드가 좀 비 효율적이라 개선하여 다시 올립니다. -한지석-

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
@@ -14,77 +14,20 @@ public class Application2 {
 		int price = sc.nextInt();
 
 		int change = amount - price;
-		int om = 0;
-		int m = 0;
-		int oc = 0;
-		int c = 0;
-		int ob = 0;
-		int b = 0;
-		int os = 0;
-		int s = 0;
 
+		int[] money = { 50000, 10000, 5000, 1000, 500, 100, 50, 10};
+		int[] moneyCount = new int[money.length];
 
-		while ( change > 0) {
-			if( change >= 50000 ) {
-				change = change - 50000;
-				om++;
-			} else if ( change >= 10000) {
-				change = change - 10000;
-				m++;
-			} else if ( change >= 5000) {
-				change = change - 5000;
-				oc++;
-			} else if ( change >= 1000) {
-				change = change - 1000;
-				c++;
-			} else if ( change >= 500) {
-				change = change - 500;
-				ob++;
-			} else if ( change >= 100) {
-				change = change - 100;
-				b++;
-			} else if ( change >= 50) {
-				change = change - 50;
-				os++;
-			} else if ( change >= 10) {
-				change = change - 10;
-				s++;
-			} else break;
+		for(int i = 0; i < money.length; i ++){
+			moneyCount[i] = change / money[i];
+			change %= money[i];
+
+			if( moneyCount[i] > 0 ){
+				if( money[i] >= 1000) {
+					System.out.println(money[i] + "원권 지폐 : " + moneyCount[i] + "장");
+				} else System.out.println(money[i] + "원권 동전 :" + moneyCount[i] + "개");
+			}
 		}
-		System.out.println("==================");
-		System.out.println("50000권 지폐 :" + om + "장");
-		System.out.println("10000권 지폐:" + m + "장");
-		System.out.println("5000권 지폐 :" + oc + "장");
-		System.out.println("1000권 지폐 :" + c + "장");
-		System.out.println("500권 동전 :" + ob + "개");
-		System.out.println("100권 동전 :" + b + "개");
-		System.out.println("50권 동전 :" + os + "개");
-		System.out.println("10권 동전 :" + s + "개");
-		System.out.println("==================");
-		System.out.println("거스름돈 :" + (amount - price) + "원");
-		/* 받은 금액을 입력 받고, 상품 가격을 입력 받아 거스름돈을 계산하는 프로그램을 작성하세요
-		 * 
-		 * 단, 거스름돈은 대한민국 화폐 단위 별 필요 수량이 출력되도록 작성하고,
-		 * 지폐외 동전을 구분하여 단위를 표기하세요
-		 * 
-		 * -- 입력 예시 --
-		 * 받으신 금액을 입력하세요 : 100000
-		 * 상품 가격을 입력하세요 : 22340
-		 * 
-		 * -- 출력 예시 --
-		 * ============================
-		 * 50000원권 지폐 1장
-		 * 10000원권 지폐 2장
-		 * 5000원권 지폐 1장
-		 * 1000원권 지폐 2장
-		 * 500원권 동전 1개
-		 * 100원권 동전 1개
-		 * 50원권 동전 1개
-		 * 10원권 동전 1개
-		 * ============================
-		 * 거스름돈 : 77660원
-		 * */
-
 	}
 
 }
