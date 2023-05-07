d148ce1f3f676c8aead8613719a8adf34697cc3b
phj9745 <117703172+phj9745@users.noreply.github.com>
2023-05-08 02:37:06 +0900

    다 풀었따

--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application1.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application1.java
@@ -1,34 +1,58 @@
 package com.ohgiraffers.section02.looping_and_branching.level04.advanced;
 
+import java.util.Scanner;
+
 public class Application1 {
-	
-	public static void main(String[] args) {
-		
-		/* 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서
-		 * 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
-		 * 
-		 * A를 3만큼 밀면 D가 되고, z를 1만큼 밀면 a가 됩니다.
-		 * 또한 공백문자는 변경되지 않습니다.
-		 * 
-		 * 문자열 1개를 입력 받고, 얼마나 문자를 밀지 알려주는 0보다 큰 정수를 1개 입력 받아
-		 * 암호문을 작성하는 프로그램을 만들어보세요
-		 * 단, 숫자가 27인 경우는 1칸을 밀게 됩니다. (알파벳은 26글자 이기 때문)
-		 * 
-		 * -- 입력 예시 --
-		 * 문자열을 입력하세요 : a B z
-		 * 숫자를 입력하세요 : 4
-		 * 
-		 * -- 출력 예시 --
-		 * e F d
-		 * 
-		 * -- 입력 예시 --
-		 * 문자열을 입력하세요 : a B z
-		 * 숫자를 입력하세요 : 27
-		 * 
-		 * -- 출력 예시 --
-		 * b C a
-		 * */
-		
-	}
-	
+
+    public static void main(String[] args) {
+
+        /* 어떤 문장의 각 알파벳을 일정한 거리만큼 밀어서
+         * 다른 알파벳으로 바꾸는 암호화 방식을 시저 암호라고 합니다.
+         *
+         * A를 3만큼 밀면 D가 되고, z를 1만큼 밀면 a가 됩니다.
+         * 또한 공백문자는 변경되지 않습니다.
+         *
+         * 문자열 1개를 입력 받고, 얼마나 문자를 밀지 알려주는 0보다 큰 정수를 1개 입력 받아
+         * 암호문을 작성하는 프로그램을 만들어보세요
+         * 단, 숫자가 27인 경우는 1칸을 밀게 됩니다. (알파벳은 26글자 이기 때문)
+         *
+         * -- 입력 예시 --
+         * 문자열을 입력하세요 : a B z
+         * 숫자를 입력하세요 : 4
+         *
+         * -- 출력 예시 --
+         * e F d
+         *
+         * -- 입력 예시 --
+         * 문자열을 입력하세요 : a B z
+         * 숫자를 입력하세요 : 27
+         *
+         * -- 출력 예시 --
+         * b C a
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+
+        System.out.println("문자열을 입력하세요 : ");
+        String str = sc.nextLine();
+        System.out.println("숫자를 입력하세요 : ");
+        int num = sc.nextInt();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch += (num % 26);
+                if (ch > 'Z') ch = (char) ('A' + (num % 26 - 1));
+                result.append(ch);
+            } else if (ch >= 'a' && ch <= 'z') {
+                ch += (num % 26);
+                if (ch > 'z') ch = (char) ('a' + (num % 26) - 1);
+                result.append(ch);
+            } else if (ch == ' ') {
+                result.append(" ");
+            }
+        }
+        System.out.println(result);
+    }
 }
--- src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
+++ src/main/java/com/ohgiraffers/section02/looping_and_branching/level04/advanced/Application2.java
@@ -1,32 +1,62 @@
 package com.ohgiraffers.section02.looping_and_branching.level04.advanced;
 
+import java.util.Scanner;
+
 public class Application2 {
 
-	public static void main(String[] args) {
-		
-		/* 받은 금액을 입력 받고, 상품 가격을 입력 받아 거스름돈을 계산하는 프로그램을 작성하세요
-		 * 
-		 * 단, 거스름돈은 대한민국 화폐 단위 별 필요 수량이 출력되도록 작성하고,
-		 * 지폐외 동전을 구분하여 단위를 표기하세요
-		 * 
-		 * -- 입력 예시 --
-		 * 받으신 금액을 입력하세요 : 100000
-		 * 상품 가격을 입력하세요 : 22340
-		 * 
-		 * -- 출력 예시 --
-		 * ============================
-		 * 50000원권 지폐 1장
-		 * 10000원권 지폐 2장
-		 * 5000원권 지폐 1장
-		 * 1000원권 지폐 2장
-		 * 500원권 동전 1개
-		 * 100원권 동전 1개
-		 * 50원권 동전 1개
-		 * 10원권 동전 1개
-		 * ============================
-		 * 거스름돈 : 77660원
-		 * */
-		
-	}
+    public static void main(String[] args) {
+
+        /* 받은 금액을 입력 받고, 상품 가격을 입력 받아 거스름돈을 계산하는 프로그램을 작성하세요
+         *
+         * 단, 거스름돈은 대한민국 화폐 단위 별 필요 수량이 출력되도록 작성하고,
+         * 지폐외 동전을 구분하여 단위를 표기하세요
+         *
+         * -- 입력 예시 --
+         * 받으신 금액을 입력하세요 : 100000
+         * 상품 가격을 입력하세요 : 22340
+         *
+         * -- 출력 예시 --
+         * ============================
+         * 50000원권 지폐 1장
+         * 10000원권 지폐 2장
+         * 5000원권 지폐 1장
+         * 1000원권 지폐 2장
+         * 500원권 동전 1개
+         * 100원권 동전 1개
+         * 50원권 동전 1개
+         * 10원권 동전 1개
+         * ============================
+         * 거스름돈 : 77660원
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("받으신 금액을 입력하세요 : ");
+        int pay = sc.nextInt();
+        System.out.print("상품 가격을 입력하세요 : ");
+        int price = sc.nextInt();
+
+        int change = pay - price;
+
+        int sinsaimdang = change / 50000;
+        int sejong = change % 50000 / 10000;
+        int ee = change % 10000 / 5000;
+        int ehwang = change % 5000 / 1000;
+        int hak = change % 1000 / 500;
+        int leesunsin = change % 500 / 100;
+        int fifty = change % 100 / 50;
+        int ten = change % 50 / 10;
+
+        System.out.println("============================");
+        System.out.println("50000원권 지폐 " + sinsaimdang + "장");
+        System.out.println("10000원권 지폐 " + sejong + "장");
+        System.out.println("5000원권 지폐 " + ee + "장");
+        System.out.println("1000원권 지폐 " + ehwang + "장");
+        System.out.println("500원권 동전 " + hak + "개");
+        System.out.println("100원권 동전 " + leesunsin + "개");
+        System.out.println("50원권 동전 " + fifty + "개");
+        System.out.println("10원권 동전 " + ten + "개");
+        System.out.println("============================");
+        System.out.println("거스름돈 : " + change + "원");
+    }
 
 }
